b2ce34a387a1bd0bae1485e8a73f67aa8c7c2564
Frank Ham <ham@cascadetechnologies.com>
2022-06-01 14:10:09 -0700

    fix bisection math and add my standard keyboard cursor keys

--- js/viewbox.js
+++ js/viewbox.js
@@ -11,16 +11,16 @@ var yb = viewbox.height;    // bottom side y component
 
 ctx.fillStyle = "rgb(3, 160, 256, 0.2)";
 ctx.strokeStyle = "rgb(3, 160, 256, 0.8)";
-ctx.fillRect(xl, yt, xr, yb);
-ctx.strokeRect(xl, yt, xr, yb);
+ctx.fillRect(xl, yt, xr-xl, yb-yt);
+ctx.strokeRect(xl, yt, xr-xl, yb-yt);
 
 // redraw function
 function redraw() {
 
     ctx.clearRect(0, 0, viewbox.width, viewbox.height);
 
-    ctx.fillRect(xl, yt, xr, yb);
-    ctx.strokeRect(xl, yt, xr, yb);
+    ctx.fillRect(xl, yt, xr-xl, yb-yt);
+    ctx.strokeRect(xl, yt, xr-xl, yb-yt);
 }
 
 
@@ -28,14 +28,14 @@ const html = document.getElementById('html');
 html.addEventListener('keydown', function(event) {
     console.log('keydown: '+event.keyCode);
 
-    if (event.keyCode == 78) {
+    if (event.keyCode == 78 || event.keyCode == 37) {
         xr = (xl+xr)/2;
-    } else if (event.keyCode == 69) {
+    } else if (event.keyCode == 69 || event.keyCode == 39) {
         xl = (xl+xr)/2;
-    }else if (event.keyCode == 73) {
-        yb = (yb-yt)/2;
-    }else if (event.keyCode == 79) {
-        yt = yt+(yb-yt)/2;
+    }else if (event.keyCode == 73 || event.keyCode == 38) {
+        yb = (yb+yt)/2;
+    }else if (event.keyCode == 79 || event.keyCode == 40) {
+        yt = (yb+yt)/2;
     } else if (event.keyCode == 32) {
             xl = 0;
             yt = 0;
